58617edff21e65ebb6c2195db0e3dce7c2c71dfb
kaleem <kaleemullahm@outlook.com>
2026-03-18 17:15:03 +0100

    new benchmark files

---
+++ benchmark hc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --partition=genoa
+#SBATCH --job-name=paramsweep
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=128
+#SBATCH --time=01:00:00
+#SBATCH --output=slurm_output_%A.out
+
+module purge
+module load 2025
+module load Anaconda3/2025.06-1
+
+source env/bin/activate
+srun python main.py
+
