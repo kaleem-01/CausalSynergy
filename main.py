98ac8ecaf28ee32964831072d7765f69b7f6d29f
kaleem <kaleemullahm@outlook.com>
2026-03-24 11:11:44 +0100

    ea_fes_pid

--- main.py
+++ main.py
@@ -78,8 +78,8 @@ if __name__ == "__main__":
         # 'hc': run_hc,
         # "ea_ues":  partial(run_ea, population_size=30, generations=None, crossover_method='edge_swap', informed_ratio=0),
         # "ea_ies_pid":  partial(run_ea, population_size=30, generations=None, crossover_method='edge_swap', informed_ratio=0.5),
-        # "ea_fes_pid":  partial(run_ea, population_size=30, generations=None, crossover_method='edge_swap', informed_ratio=1),
-        "ea_fg_pid":  partial(run_ea, population_size=30, generations=20, informed_ratio=1, final_greedy=True),
+        "ea_fes_pid":  partial(run_ea, population_size=30, generations=None, crossover_method='edge_swap', informed_ratio=1),
+        # "ea_fg_pid":  partial(run_ea, population_size=30, generations=20, informed_ratio=1, final_greedy=True),
         # "ea_hc": partial(run_ea, population_size=1, generations=1, crossover_method='edge_swap', informed_ratio=1, final_greedy=True),
         # "notears": run_notears
     }
